da167fe9228f66893624db60be09d1b03f4d88b2
Prince Bansal <princebansal42@gmail.com>
2021-06-23 02:31:53 +0530

    Add content.js

---
+++ content.js
@@ -0,0 +1,12 @@
+document.querySelector('[name="btnK"]').click();
+setTimeout(() => {
+    let btn = document.querySelector("[name='gs_lcp']");
+    let prev = btn.value;
+    setInterval(() => {
+        if(btn.value !== prev) {
+            console.log("PRINT");
+            prev = btn.value;
+        }
+    }, 500);
+    
+}, 1000);
