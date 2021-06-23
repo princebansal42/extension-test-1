75ba9bbea0f71cb5af3f5faf43fbce338386bce7
princebansal42 <princebansal42@gmail.com>
2021-06-24 03:57:48 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,11 @@
 document.querySelector('[name="btnK"]').click();
-// setTimeout(() => {
-let btn = document.querySelector("[name='gs_lcp']");
-let prev = btn.value;
-setInterval(() => {
-    if(btn.value !== prev) {
-        console.log("PRINT");
-        prev = btn.value;
-    }
-}, 500);
-    
-// }, 1000);
+setTimeout(() => {
+    let btn = document.querySelector("[name='gs_lcp']");
+    let prev = btn.value;
+    setInterval(() => {
+        if(btn.value !== prev) {
+            console.log("PRINT");
+            prev = btn.value;
+        }
+    }, 500);
+}, 1000);
