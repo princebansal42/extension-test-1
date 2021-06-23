398b13c5d383a609df95283e6523c74270e07d54
Prince Bansal <princebansal42@gmail.com>
2021-06-24 04:34:55 +0530

    add approach 2

--- content.js
+++ content.js
@@ -1,12 +1,24 @@
+// Approach 1
 document.querySelector('[name="btnK"]').click();
-// setTimeout(() => {
+setTimeout(() => {
 let btn = document.querySelector("[name='gs_lcp']");
 let prev = btn.value;
 setInterval(() => {
     if(btn.value !== prev) {
-        console.log("PRINT");
+        console.log("1. WE ARE PRINTING SOMETHING ON CONSOLE");
         prev = btn.value;
     }
 }, 500);
     
-// }, 1000);
+}, 1000);
+
+
+// Approach 2
+let btn1 = document.createElement('button');
+btn1.setAttribute("id", "new-btn");
+btn1.addEventListener('click', function() {
+    console.log("2. WE ARE PRINTING SOMETHING ON CONSOLE")
+})
+
+let a = document.querySelector('.o3j99.ikrT4e.om7nvf');
+a.parentNode.insertBefore(btn1, a);
